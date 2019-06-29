0d84fe33df212f4dbbd283b7f1293c96f96cb735
alexs <alexs75@yandex.ru>
2019-06-29 18:35:53 +0300

    forgot files

---
+++ sql_script/fbmsqlscriptrunquestionunit.pas
@@ -0,0 +1,33 @@
+unit fbmSQLScriptRunQuestionUnit;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ButtonPanel;
+
+type
+
+  { TfbmSQLScriptRunQuestionForm }
+
+  TfbmSQLScriptRunQuestionForm = class(TForm)
+    ButtonPanel1: TButtonPanel;
+    RadioButton1: TRadioButton;
+    RadioButton2: TRadioButton;
+    RadioButton3: TRadioButton;
+  private
+
+  public
+
+  end;
+
+var
+  fbmSQLScriptRunQuestionForm: TfbmSQLScriptRunQuestionForm;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
