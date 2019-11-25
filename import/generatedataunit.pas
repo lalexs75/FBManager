4c3412e522a75efdb546d8c9a63709fc10624b4e
alexs <alexs75@yandex.ru>
2019-11-25 22:08:40 +0300

    continue work on test data generator

--- import/generatedataunit.pas
+++ import/generatedataunit.pas
@@ -26,32 +26,55 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ButtonPanel, ExtCtrls,
-  StdCtrls, Spin, ActnList, Menus, DB, rxmemds, rxdbgrid, SQLEngineAbstractUnit;
+  StdCtrls, Spin, ActnList, Menus, DBCtrls, DB, rxmemds, rxdbgrid,
+  SQLEngineAbstractUnit;
 
 type
 
   { TGenerateDataForm }
 
   TGenerateDataForm = class(TForm)
+    ComboBox1: TComboBox;
+    ComboBox2: TComboBox;
+    DBMemo1: TDBMemo;
     fldSelAll: TAction;
     fldUnSelAll: TAction;
     ActionList1: TActionList;
     ButtonPanel1: TButtonPanel;
     dsFields: TDataSource;
     GroupBox1: TGroupBox;
+    GroupBox2: TGroupBox;
+    GroupBox3: TGroupBox;
+    GroupBox4: TGroupBox;
     Label1: TLabel;
     Label2: TLabel;
     Label3: TLabel;
     Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
     MenuItem1: TMenuItem;
     MenuItem2: TMenuItem;
     MenuItem3: TMenuItem;
     PopupMenu1: TPopupMenu;
     RadioGroup1: TRadioGroup;
-    RadioGroup2: TRadioGroup;
+    RadioGroup2: TDBRadioGroup;
     RxDBGrid1: TRxDBGrid;
     rxFields: TRxMemoryData;
     rxFieldsCHEKED: TBooleanField;
+    rxFieldsDataGenAsGUID: TBooleanField;
+    rxFieldsDataGenDateIncludeTime: TBooleanField;
+    rxFieldsDataGenDateMax: TDateTimeField;
+    rxFieldsDataGenDateMin: TDateTimeField;
+    rxFieldsDataGenExtFieldName: TStringField;
+    rxFieldsDataGenExtTableName: TStringField;
+    rxFieldsDataGenIntMax: TLongintField;
+    rxFieldsDataGenIntMin: TLongintField;
+    rxFieldsDataGenSimpleList: TMemoField;
+    rxFieldsDataGenStrEndChar: TStringField;
+    rxFieldsDataGenStrMaxLen: TLongintField;
+    rxFieldsDataGenStrMinLen: TLongintField;
+    rxFieldsDataGenStrStartChar: TStringField;
+    rxFieldsDataGenType: TLongintField;
     rxFieldsFieldName: TStringField;
     rxFieldsFieldType: TStringField;
     rxFieldsFieldTypeInt: TLongintField;
@@ -61,10 +84,14 @@ type
     SpinEdit4: TSpinEdit;
     procedure fldSelAllExecute(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure RadioGroup2Change(Sender: TObject);
+    procedure rxFieldsAfterScroll(DataSet: TDataSet);
+    procedure rxFieldsBeforeScroll(DataSet: TDataSet);
   private
     FTable: TDBTableObject;
     procedure Localize;
     procedure LoadTableInfo;
+    procedure UpdateEditControl;
   public
     constructor CreateGenerateDataForm(ATable:TDBTableObject);
     //function SaveData:boolean;
@@ -99,6 +126,24 @@ begin
   SpinEdit4.Value:=MaxInt;
 end;
 
+procedure TGenerateDataForm.RadioGroup2Change(Sender: TObject);
+begin
+  if rxFields.State <> dsBrowse then
+    rxFields.Post;
+
+  UpdateEditControl;
+end;
+
+procedure TGenerateDataForm.rxFieldsAfterScroll(DataSet: TDataSet);
+begin
+  UpdateEditControl;
+end;
+
+procedure TGenerateDataForm.rxFieldsBeforeScroll(DataSet: TDataSet);
+begin
+  //
+end;
+
 procedure TGenerateDataForm.Localize;
 begin
 
@@ -108,6 +153,10 @@ procedure TGenerateDataForm.LoadTableInfo;
 var
   F: TDBField;
 begin
+  rxFields.BeforeScroll:=nil;
+  rxFields.AfterScroll:=nil;
+
+  rxFields.DisableControls;
   rxFields.CloseOpen;
   if not Assigned(FTable) then Exit;
   for F in FTable.Fields do
@@ -117,10 +166,47 @@ begin
     rxFieldsFieldName.AsString:=F.FieldName;
     rxFieldsFieldType.AsString:=F.TypeStr;
     rxFieldsFieldTypeInt.AsInteger:=Ord(F.FieldTypeDB);
+    rxFieldsDataGenType.AsInteger:=0;
     rxFields.Post;
   end;
 
   rxFields.First;
+  rxFields.EnableControls;
+
+  rxFields.BeforeScroll:=@rxFieldsBeforeScroll;
+  rxFields.AfterScroll:=@rxFieldsAfterScroll;
+
+  UpdateEditControl;
+end;
+
+procedure TGenerateDataForm.UpdateEditControl;
+
+procedure DoUpdate(P, SP:TGroupBox);
+begin
+  P.Visible:=P = SP;
+  if P.Visible then
+  begin
+    P.AnchorSideTop.Control:=RadioGroup2;
+    P.AnchorSideLeft.Control:=RadioGroup2;
+    P.AnchorSideRight.Control:=RadioGroup2;
+  end;
+end;
+
+var
+  G: TGroupBox;
+begin
+  case rxFieldsDataGenType.AsInteger of
+    0:G:=GroupBox1;
+    1:G:=GroupBox2;
+    2:G:=GroupBox3;
+    3:G:=GroupBox4;
+  else
+    G:=nil;
+  end;
+  DoUpdate(GroupBox1, G);
+  DoUpdate(GroupBox2, G);
+  DoUpdate(GroupBox3, G);
+  DoUpdate(GroupBox4, G);
 end;
 
 constructor TGenerateDataForm.CreateGenerateDataForm(ATable: TDBTableObject);
