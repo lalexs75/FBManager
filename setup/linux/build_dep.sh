259cd1f88aea08ae82385cffdb81888af3e36179
lalexs75 <alexs75@yandex.ru>
2023-07-31 16:55:28 +0300

    continue work on deb install script

--- setup/linux/build_dep.sh
+++ setup/linux/build_dep.sh
@@ -1,20 +1,35 @@
 mkdir -p ./package/DEBIAN
 mkdir -p ./package/usr/bin
 mkdir -p ./package/usr/share/applications
+mkdir -p ./package/usr/share/pixmaps
 
-cp /home/alexs/1/IBManager ./package/usr/bin
+cd ../../
+./make_fbm.sh gtk2
+cd setup/linux
+
+cp ../../IBManager ./package/usr/bin
 cp ./FBManager.desktop ./package/usr/share/applications
+cp ../../Images/icons/MainIcon.png ./package/usr/share/pixmaps
+
 echo 'Package: fbmanager
 Version: 0.1
 Section: admin,devel
 Priority: optional
-Depends: libc6, libpangocairo-1
+Depends: libc6, libpangocairo-1.0-0, libatk1.0-0
 Architecture: amd64
 Essential: no
+Suggests: sqlite3,libpq-dev
+Origin: https://github.com/lalexs75/FBManager
 Maintainer: Lagunov Aleksey <alexs75@yandex.ru>
 Description: FBManager is a universal managment database application. It''s support Postgre SQL, MySQL, FireBird SQL, SQLite3.' >package/DEBIAN/control 
 
 #Installed-Size: 20
 
-dpkg-deb --build ./package
+
+echo 'if [ "$1" = "configure" ] && [ -x "`which update-menus 2>/dev/null`" ] ; then
+update-menus
+fi' > package/DEBIAN/postinst
+chmod 0755 package/DEBIAN/postinst
+fakeroot dpkg-deb --build ./package
 rm -r ./package
+mv package.deb fbmanager.deb
