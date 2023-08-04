9e76d184ffe0193f7391773e3958c20e1a3bd620
lalexs75 <alexs75@yandex.ru>
2023-08-04 13:37:57 +0300

    deb installer - add full size

--- setup/linux/build_dep.sh
+++ setup/linux/build_dep.sh
@@ -18,19 +18,24 @@ cp -a ../../languages/* ./package/usr/share/fbmanager/languages
 cp -a ../../reports/* ./package/usr/share/fbmanager/reports
 cp -a ../../docs/* ./package/usr/share/fbmanager/docs
 
+dir_size1=`du -sb ./package`
+dir_size=${dir_size1:0:`expr index "$dir_size1" "./"`-1}
+
+
 echo 'Package: fbmanager
 Version: 0.1
 Section: admin,devel
 Priority: optional
 Depends: libc6, libpangocairo-1.0-0, libatk1.0-0
 Architecture: amd64
-Essential: no
-Suggests: sqlite3,libpq-dev
+Essential: no'  >package/DEBIAN/control 
+echo 'Installed-Size: ' $dir_size >>package/DEBIAN/control 
+
+echo 'Suggests: sqlite3,libpq-dev
 Origin: https://github.com/lalexs75/FBManager
 Maintainer: Lagunov Aleksey <alexs75@yandex.ru>
-Description: FBManager is a universal managment database application. It''s support Postgre SQL, MySQL, FireBird SQL, SQLite3.' >package/DEBIAN/control 
+Description: FBManager is a universal managment database application. It''s support Postgre SQL, MySQL, FireBird SQL, SQLite3.' >>package/DEBIAN/control 
 
-#Installed-Size: 20
 
 
 echo 'if [ "$1" = "configure" ] && [ -x "`which update-menus 2>/dev/null`" ] ; then
