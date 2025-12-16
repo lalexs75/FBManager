4fcfcd5548b298790e4280e564beb6af425bdf87
lalexs75 <alexs75@yandex.ru>
2025-12-16 10:40:30 +0300

    Update i18n files

--- update_lng_files.sh
+++ update_lng_files.sh
@@ -1,10 +1,12 @@
-cp /usr/local/share/lazarus/components/dbexport/languages/*.po                                    /home/alexs/work/FBManager/languages/dbexport/
-cp /usr/local/share/lazarus/components/FBDataSet/languages/*.po                                   /home/alexs/work/FBManager/languages/fbdataset/
-cp /usr/local/share/lazarus/components/fpspreadsheet/languages/*.po                               /home/alexs/work/FBManager/languages/fpspreadsheet/
-cp /usr/local/share/lazarus/components/lazreport/source/languages/*.po                            /home/alexs/work/FBManager/languages/lazreport/
-cp /usr/local/share/lazarus/components/lazreport/source/addons/addfunction/languages/*.po         /home/alexs/work/FBManager/languages/lazreport/
-cp /usr/local/share/lazarus/components/lazreport/source/addons/ZeosDB/languages/*.po              /home/alexs/work/FBManager/languages/lazreport/
-cp /usr/local/share/lazarus/components/lazreport/source/addons/lrEmailExport/languages/*.po       /home/alexs/work/FBManager/languages/lazreport/
-cp /usr/local/share/lazarus/components/lazreport/source/addons/lrspreadsheetexport/languages/*.po /home/alexs/work/FBManager/languages/lazreport/
-cp /usr/local/share/lazarus/lcl/languages/*.po                                                    /home/alexs/work/FBManager/languages/lcl/
-cp /usr/local/share/lazarus/components/rxnew/languages/*.po                                       /home/alexs/work/FBManager/languages/rx/
+cur_dir=`pwd`
+echo $cur_dir
+cp /usr/local/share/lazarus/components/dbexport/languages/*.po                                    $cur_dir/languages/dbexport/
+cp /usr/local/share/lazarus/components/FBDataSet/languages/*.po                                   $cur_dir/languages/fbdataset/
+cp /usr/local/share/lazarus/components/fpspreadsheet/languages/*.po                               $cur_dir/languages/fpspreadsheet/
+cp /usr/local/share/lazarus/components/lazreport/source/languages/*.po                            $cur_dir/languages/lazreport/
+cp /usr/local/share/lazarus/components/lazreport/source/addons/addfunction/languages/*.po         $cur_dir/languages/lazreport/
+cp /usr/local/share/lazarus/components/lazreport/source/addons/ZeosDB/languages/*.po              $cur_dir/languages/lazreport/
+cp /usr/local/share/lazarus/components/lazreport/source/addons/lrEmailExport/languages/*.po       $cur_dir/languages/lazreport/
+cp /usr/local/share/lazarus/components/lazreport/source/addons/lrspreadsheetexport/languages/*.po $cur_dir/languages/lazreport/
+cp /usr/local/share/lazarus/lcl/languages/*.po                                                    $cur_dir/languages/lcl/
+cp /usr/local/share/lazarus/components/rxnew/languages/*.po                                       $cur_dir/languages/rx/
